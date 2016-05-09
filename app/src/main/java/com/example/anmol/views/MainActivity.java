cf753632b9678ce6ffb24554183006150be02344
anmol92verma <anmol.verma4@gmail.com>
2016-05-09 21:43:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/anmol/views/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.anmol.views;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.inputmethod.InputMethodManager;
+
+import com.example.anmol.RepositoryAdapter;
+import com.example.anmol.model.Repository;
+import com.example.anmol.trendmvvm.R;
+import com.example.anmol.trendmvvm.databinding.ActivityMainBinding;
+import com.example.anmol.viewmodel.MainViewModel;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainViewModel.DataListener {
+
+    private ActivityMainBinding binding;
+    private MainViewModel mainViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainViewModel = new MainViewModel(this, this);
+        binding.setViewModel(mainViewModel);
+        setSupportActionBar(binding.toolbar);
+        setupRecyclerView(binding.reposRecyclerView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mainViewModel.destroy();
+    }
+
+    @Override
+    public void onRepositoriesChanged(List<Repository> repositories) {
+        RepositoryAdapter adapter =
+                (RepositoryAdapter) binding.reposRecyclerView.getAdapter();
+        adapter.setRepositories(repositories);
+        adapter.notifyDataSetChanged();
+        hideSoftKeyboard();
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        RepositoryAdapter adapter = new RepositoryAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void hideSoftKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(binding.editTextUsername.getWindowToken(), 0);
+    }
+
+}
